babbb4b73909a13e18c7f683535b3b3c657bd14c
dembasy <dembasy1227@gmail.com>
2018-07-31 15:47:10 +0200

    done

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$region = array(
+// ajout du numero de département de Reims
+  '59' => 'Nord',
+  '62' => 'Pas de Calais',
+  '02'=> 'Aisne',
+  '60' => 'Oise',
+  '80' => 'Somme',
+  '51' => 'Champagne Ardennes');
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title> exercice 7 </title>
+  </head>
+  <body>
+  </body>
+</html>
